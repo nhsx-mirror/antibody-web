bf763978656f647c51c3324dca02970d62f7bf6c
Daniel Burnley <dan@danops.uk>
2020-06-19 11:52:32 +0100

    Add testutil for rendering with a stub app context

--- client/src/utils/testUtils.tsx
+++ client/src/utils/testUtils.tsx
@@ -4,11 +4,37 @@ import { render } from '@testing-library/react';
 import { IntlProvider } from 'react-intl';
 import messages from 'i18n/index';
 import { flatten } from 'flat';
+import AppContext from 'components/App/context';
 
 export const renderWithReactIntl = component => {
-  return render(<IntlProvider 
-    locale="en-gb" 
+  return render(<IntlProvider
+    locale="en-gb"
     messages={flatten(messages["en-gb"])}>
     {component}
   </IntlProvider>);
+};
+
+export const renderWithStubAppContext = component => {
+  const appContext = {
+    state: { locale: "en-gb" },
+    setLocale: () => { },
+    setAppError: () => { },
+    dispatch: () => { },
+    container: {
+      getLogin: () => () => { },
+      getTestApi: () => ({
+        generateTest: () => ({ testRecord: { timerStartedAt: 10 } })
+      })
+    }
+  };
+
+  return render(
+    <AppContext.Provider value={appContext} >
+      <IntlProvider
+        locale="en-gb"
+        messages={flatten(messages["en-gb"])}>
+        {component}
+      </IntlProvider>
+    </AppContext.Provider >
+  );
 };
